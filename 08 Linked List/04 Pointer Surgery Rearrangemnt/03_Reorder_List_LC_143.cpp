28cb9b53dd725298aadee2125943863080e334bb
Lakshay Meena <lakshaymeena171@gmail.com>
2026-09-19 15:27:11 +0530

    add reorder LL solution

---
+++ 08 Linked List/04 Pointer Surgery Rearrangemnt/03_Reorder_List_LC_143.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
+Node *reverseList(Node *head)
+{
+    Node *prev = nullptr;
+    Node *curr = head;
+
+    while (curr != nullptr)
+    {
+        Node *nextNode = curr->next;
+
+        curr->next = prev;
+
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+}
+
+void reorderList(Node *head)
+{
+    // Empty ya single node
+    if (head == nullptr || head->next == nullptr)
+    {
+        return;
+    }
+
+    // STEP 1: Find middle
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast->next != nullptr && fast->next->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // STEP 2: Split into two halves
+
+    Node *second = slow->next;
+
+    slow->next = nullptr;
+
+    // STEP 3: Reverse second half
+
+    second = reverseList(second);
+
+    // STEP 4: Alternate merge
+
+    Node *first = head;
+
+    while (second != nullptr)
+    {
+        Node *firstNext = first->next;
+        Node *secondNext = second->next;
+
+        first->next = second;
+        second->next = firstNext;
+
+        first = firstNext;
+        second = secondNext;
+    }
+}
+
+void printList(Node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+
+    cout << "NULL\n";
+}
+
+int main()
+{
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original List:\n";
+    printList(head);
+
+    reorderList(head);
+
+    cout << "\nReordered List:\n";
+    printList(head);
+
+    return 0;
+}
