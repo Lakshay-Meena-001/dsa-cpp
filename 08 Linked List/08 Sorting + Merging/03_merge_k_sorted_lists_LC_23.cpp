41c212bde2a01c1e133265a7e0cf70be7b39e06f
Lakshay Meena <lakshaymeena171@gmail.com>
2026-09-22 11:38:02 +0530

    add merge k sorted lists solution

---
+++ 08 Linked List/08 Sorting + Merging/03_merge_k_sorted_lists_LC_23.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int data;
+    ListNode *next;
+
+    ListNode(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *a, ListNode *b)
+    {
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+
+        while (a != nullptr && b != nullptr)
+        {`
+            if (a->data <= b->data)
+            {
+                tail->next = a;
+                a = a->next;
+            }
+            else
+            {
+                tail->next = b;
+                b = b->next;
+            }
+
+            tail = tail->next;
+        }
+
+        if (a != nullptr)
+        {
+            tail->next = a;
+        }
+
+        if (b != nullptr)
+        {
+            tail->next = b;
+        }
+
+        return dummy.next;
+    }
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        ListNode *result = nullptr;
+
+        for (ListNode *list : lists)
+        {
+            result = mergeTwoLists(result, list);
+        }
+
+        return result;
+    }
+};
